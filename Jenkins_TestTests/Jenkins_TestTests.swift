2bd97e55bf4ee1e0a4810e0b65e055d178ea0aaf
Aaron <aarontsosa@outlook.com>
2018-01-26 10:33:45 -0500

    inital commit

---
+++ Jenkins_TestTests/Jenkins_TestTests.swift
@@ -0,0 +1,36 @@
+//
+//  Jenkins_TestTests.swift
+//  Jenkins_TestTests
+//
+//  Created by Aaron Sosa on 1/26/18.
+//  Copyright © 2018 Aaron Sosa. All rights reserved.
+//
+
+import XCTest
+@testable import Jenkins_Test
+
+class Jenkins_TestTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
